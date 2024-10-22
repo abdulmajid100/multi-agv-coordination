dd76fbbee00fe60393d3b7b1708b1cd055c116aa
majmo <abdu.777777777@gmail.com>
2024-10-22 17:41:47 +0300

    v4

---
+++ ql_100.py
@@ -0,0 +1,29 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Create a directed graph
+G = nx.DiGraph()
+
+# Add nodes
+nodes = list(range(1, 30)) + [9]
+G.add_nodes_from(nodes)
+
+# Add edges
+edges = [
+    (1, 4), (2, 4), (3, 4), (4, 1), (4, 2), (4, 3), (4, 6), (4, 5),
+    (5, 4), (6, 4), (4, 11), (11, 4), (11, 10), (11, 21), (11, 12),
+    (10, 11), (10, 20), (20, 10), (21, 11), (12, 11), (12, 22),
+    (22, 12), (12, 13), (13, 12), (13, 23), (23, 13), (13, 14),
+    (14, 13), (14, 24), (24, 14), (14, 15), (15, 14), (15, 25),
+    (25, 15), (15, 16), (16, 15), (16, 26), (26, 16), (16, 17),
+    (17, 16), (17, 27), (27, 17), (17, 18), (18, 17), (18, 28),
+    (28, 18), (18, 19), (19, 18), (19, 29), (29, 19), (9, 16),
+    (16, 9)
+]
+G.add_edges_from(edges)
+
+# Draw the graph
+plt.figure(figsize=(12, 8))
+nx.draw(G, with_labels=True, node_size=700, node_color='lightblue', font_size=10, font_weight='bold', arrows=True)
+plt.title("Directed Graph")
+plt.show()
